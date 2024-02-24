5efd4cca51c27486ad53370c35b530bdbcd4958f
Karim Ouazzou <karimouazzou@gmail.com>
2024-02-24 19:18:48 +0100

    enable cors

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express")
 const dotenv = require("dotenv")
 const process = require("process")
+const cors = require("cors")
 
 dotenv.config()
 const port = process.env.PORT
 
 const app = express()
 app.listen(port)
+
+app.use(cors())
 
 app.get("/", (req, res) => {
   res.send("App is working")
