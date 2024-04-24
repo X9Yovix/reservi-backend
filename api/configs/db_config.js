9ec09c63358557ed7c5cf6bb26605860591d7fe2
Karim Ouazzou <karimouazzou@gmail.com>
2024-04-24 02:13:00 +0100

    generic db name

--- api/configs/db_config.js
+++ api/configs/db_config.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose")
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI)
+    await mongoose.connect(process.env.MONGODB_URI,{
+      dbName: process.env.MONGODB_DB,
+    })
     console.log("Connected to MongoDB")
   } catch (error) {
     console.error("Error connecting to MongoDB", error)
